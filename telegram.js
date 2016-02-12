a213fae0c672261f4fa7d94165698cbd6e7f86ec
jdrydn <james@jdrydn.com>
2016-02-12 12:52:55 +0000

    Allowing custom bots from a database... if you're smart :wink:

--- telegram.js
+++ telegram.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var request = require('request');
 var telegram = module.exports = {};
 
@@ -10,13 +11,9 @@ telegram.configure = function (opts) {
   if (opts.hasOwnProperty('token')) DEFAULTS.token = opts.token;
 };
 
-var extendOmit = function (omit_props) {
-  var base_obj = {};
-
+var extend = function (base_obj) {
   for (var i = 1; i < arguments.length; i++) if (arguments[i]) {
-    for (var prop in arguments[i]) if (arguments[i].hasOwnProperty(prop) && omit_props.indexOf(prop) < 0) {
-      base_obj[prop] = arguments[i][prop];
-    }
+    for (var prop in arguments[i]) if (arguments[i].hasOwnProperty(prop)) base_obj[prop] = arguments[i][prop];
   }
 
   return base_obj;
@@ -28,21 +25,19 @@ telegram.send = function (opts, callback) {
   if (!opts.token) return callback(new Error('Missing `token` property'));
   if (!opts.to) return callback(new Error('Missing `to` property'));
 
+  if (!opts.method) return callback(new Error('Missing `method` property'));
+  if (typeof opts.message.method !== 'string') return callback(new Error('Incorrect type for `message` property'));
   if (!opts.message) return callback(new Error('Missing `message` property'));
   if (typeof opts.message !== 'object') return callback(new Error('Incorrect type for `message` property'));
-  if (!opts.message.method) return callback(new Error('Missing `method` property in `message` object'));
-  if (typeof opts.message.method !== 'string') return callback(new Error(
-    'Incorrect type for `message` property in `message` object'
-  ));
 
   request({
     method: 'POST',
-    url: 'https://api.telegram.org/bot' + opts.token + '/' + opts.message.method,
+    url: 'https://api.telegram.org/bot' + opts.token + '/' + opts.method,
     headers: {
       'Accept': 'application/json'
     },
     json: true,
-    formData: extendOmit([ 'method' ], opts.message, {
+    formData: extend(opts.message, {
       chat_id: opts.to
     })
   }, function (err, res, body) {
@@ -54,25 +49,29 @@ telegram.send = function (opts, callback) {
 };
 
 telegram.api = function (opts) {
+  opts = opts || {};
   if (typeof opts !== 'object') throw new Error('Invalid object of options');
-  if (typeof opts.bots !== 'object') throw new TypeError('Expected an object of bots');
 
   opts.bot_name_param = opts.bot_name_param || 'bot_name';
   opts.bot_auth_param = opts.bot_auth_param || 'bot_auth';
 
-  for (var slug in opts.bots) if (opts.bots.hasOwnProperty(slug)) {
-    if (typeof opts.bots[slug] === 'string') opts.bots[slug] = { token: opts.bots[slug] };
-    if (!opts.bots[slug].token) throw new Error('Missing token for "' + slug + '" bot');
+  if (opts.bots && typeof opts.bots === 'object') {
+    for (var slug in opts.bots) if (opts.bots.hasOwnProperty(slug)) {
+      if (typeof opts.bots[slug] === 'string') opts.bots[slug] = { token: opts.bots[slug] };
+      if (!opts.bots[slug].token) throw new Error('Missing token for "' + slug + '" bot');
 
-    opts.bots[slug].name = opts.bots[slug].name || slug;
-    opts.bots[slug].slug = slug;
+      opts.bots[slug].name = opts.bots[slug].name || slug;
+      opts.bots[slug].slug = slug;
 
-    if (opts.bots[slug].auth && typeof opts.bots[slug].auth !== 'string') {
-      throw new Error('`auth` property for "' + slug + '" should be a string');
+      if (opts.bots[slug].auth && typeof opts.bots[slug].auth !== 'string') {
+        throw new Error('`auth` property for "' + slug + '" should be a string');
+      }
     }
   }
+  else if (opts.bots) throw new Error('Bots option should be an object of bots or omitted');
 
   return function telegramWebhook(req, res, next) {
+    var bot = null;
     var err = null;
 
     if (!req.param || !req.param[opts.bot_name_param]) {
@@ -80,20 +79,31 @@ telegram.api = function (opts) {
       err.status = err.statusCode = 404;
       return next(err);
     }
+
     if (!req.body) {
       err = new Error('Missing body - did you parse the JSON body?');
       err.status = err.statusCode = 500;
       return next(err);
     }
 
-    var bot = opts.bots[req.param[opts.bot_name_param]];
+    if (
+      !req.body.message || !req.body.message.update_id || !req.body.message.message ||
+      !req.body.message.message.message_id || !req.body.message.message.from || !req.body.message.message.from.id
+    ) {
+      err = new Error('Invalid message received from Telegram');
+      err.status = err.statusCode = 400;
+      return next(err);
+    }
+
+    if (req.telegram_bot) bot = req.telegram_bot;
+    else if (opts.bots) bot = opts.bots[req.param[opts.bot_name_param]];
     if (!bot) {
       err = new Error('Unknown bot "' + req.param[opts.bot_name_param] + '"');
       err.status = err.statusCode = 404;
       return next(err);
     }
 
-    if (bot.auth) {
+    if (bot && bot.auth) {
       if (!req.param[opts.bot_auth_param]) {
         err = new Error('Missing bot auth in URL');
         err.status = err.statusCode = 401;
@@ -107,29 +117,22 @@ telegram.api = function (opts) {
       }
     }
 
-    if (
-      !req.body.message || !req.body.message.update_id || !req.body.message.message ||
-      !req.body.message.message.message_id || !req.body.message.message.from || !req.body.message.message.from.id
-    ) {
-      err = new Error('Invalid message received from Telegram');
-      err.status = err.statusCode = 400;
-      return next(err);
-    }
-
     req.telegram = {
       bot: bot,
       message: req.body.message.message,
-      reply: function (message, callback) {
-        message.reply_to_message_id = req.body.message.message.message_id;
-
-        telegram.send({
-          token: bot.token,
-          to: req.body.message.message.from.id,
-          message: message
-        }, callback);
-      },
       token: opts.token,
       update_id: req.body.message.update_id
+    };
+
+    req.telegram.reply = function (method, message, callback) {
+      message.reply_to_message_id = req.body.message.message.message_id;
+
+      telegram.send({
+        token: req.telegram.bot.token,
+        to: req.body.message.message.from.id,
+        method: method,
+        message: message
+      }, callback);
     };
 
     next();
